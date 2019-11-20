ab38941b3906f8fd587e51bb03e2a1ec0ea8c807
kuankuo123 <kuankuo123@163.com>
2019-11-20 23:36:40 +0800

    Update Demo.py

--- Demo.py
+++ Demo.py
@@ -6,7 +6,9 @@ from selenium.webdriver.common.action_chains import ActionChains
 driver = webdriver.Firefox()
 driver.maximize_window()
 #driver.get(r'C:\Users\ian\workspace\HtmlDemo\WebContent\NewFile.html')
+#0000000000000000
 time.sleep(3)
+#1111111111111111111
 #定位到要悬停的元素
 stop =driver.find_element_by_class_name("icon-arrow-down") 
 #对定位到的元素执行鼠标悬停操作
