1ad5766e34745c16a1ea6ff8088e0e00d5143cca
unknown <kuankuo123@163.com>
2019-11-20 22:39:52 +0800

    dd

---
+++ AutoDemo.py
@@ -0,0 +1,6 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import *
+
+By
---
+++ Demo.py
@@ -0,0 +1,13 @@
+
+from selenium import webdriver
+#引入ActionChains类
+from selenium.webdriver.common.action_chains import ActionChains
+
+driver = webdriver.Firefox()
+driver.maximize_window()
+#driver.get(r'C:\Users\ian\workspace\HtmlDemo\WebContent\NewFile.html')
+time.sleep(3)
+#定位到要悬停的元素
+stop =driver.find_element_by_class_name("icon-arrow-down") 
+#对定位到的元素执行鼠标悬停操作
+ActionChains(driver).context_click(stop).perform()
